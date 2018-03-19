89506de1cb9774e3eb204910d126c929941b6884
Paweł Lelental <p.lelental@gmail.com>
2018-03-19 13:52:24 +0100

    Update server.sh

--- scripts/server.sh
+++ scripts/server.sh
@@ -7,4 +7,4 @@ cd $(dirname $0)
 cd ..
 
 #in memory database
-java -cp scripts/hsqldb.jar org.hsqldb.server.Server --database.0 
+java -cp scripts/hsqldb.jar org.hsqldb.server.Server --database.0 mem:mydb --dbname.0 workdb
