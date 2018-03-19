b308a2129176d9676fba769c6dc719ee079411eb
Paweł Lelental <p.lelental@gmail.com>
2018-03-19 13:53:24 +0100

    Update client.sh

--- scripts/client.sh
+++ scripts/client.sh
@@ -3,10 +3,7 @@
 cd $(dirname $0)
 
 
-if [ ! -f hsqldb.jar ]; then
-    wget http://puzniakowski.pl/repo/libs/hsqldb.jar
-fi
 
 cd ..
 
-java -cp scripts/hsqldb.jar org.hsqldb.util.DatabaseManagerSwing --url jdbc:hsqldb:hsql://localhost/
+java -cp scripts/hsqldb.jar org.hsqldb.util.DatabaseManagerSwing --url jdbc:hsqldb:hsql://localhost/workdb
