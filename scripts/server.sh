305d06dd040dd2e87f37b25db7bacc6101e4c787
veok <p.lelental@gmail.com>
2018-03-25 12:50:52 +0200

    tests

--- scripts/server.sh
+++ scripts/server.sh
@@ -3,7 +3,10 @@
 
 cd $(dirname $0)
 
+if [ ! -f hsqldb.jar ]; then
+    wget http://puzniakowski.pl/repo/libs/hsqldb.jar
+fi
 
 cd ..
 
-java -cp scripts/hsqldb.jar org.hsqldb.server.Server --database.0 mem:mydb --dbname.0 workdb
+java -cp scripts/hsqldb.jar org.hsqldb.server.Server --database.0 mem:mydb --dbname.0 workdb &
