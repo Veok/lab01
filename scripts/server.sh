321a14b639fc622f6d23e1dc040a8ee2eaf645ec
veok <p.lelental@gmail.com>
2018-03-25 12:45:55 +0200

    tests

--- scripts/server.sh
+++ scripts/server.sh
@@ -6,4 +6,4 @@ cd $(dirname $0)
 
 cd ..
 
-java -cp scripts/hsqldb.jar org.hsqldb.server.Server --database.0 mem:mydb --dbname.0
+java -cp scripts/hsqldb.jar org.hsqldb.server.Server --database.0 mem:mydb --dbname.0 workdb
